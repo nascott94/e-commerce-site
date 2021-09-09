7f48dc3fd37b40850d7883cbe6cd11807ad63ce8
nascott <nascott94@outlook.com>
2021-09-08 20:56:20 -0600

    fixed bug

--- config/connection.js
+++ config/connection.js
@@ -1,12 +1,16 @@
-require('dotenv').config();
+require("dotenv").config();
 
-const Sequelize = require('sequelize');
-
+const Sequelize = require("sequelize");
+console.log({
+  db_name: process.env.DB_NAME,
+  db_user: process.env.DB_USER,
+  db_pass: process.env.DB_PW,
+});
 const sequelize = process.env.JAWSDB_URL
   ? new Sequelize(process.env.JAWSDB_URL)
   : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
-      host: 'localhost',
-      dialect: 'mysql',
+      host: "localhost",
+      dialect: "mysql",
       dialectOptions: {
         decimalNumbers: true,
       },
